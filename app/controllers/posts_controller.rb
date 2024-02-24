99059f61a32466e2ae29c9ba730ec8eeaf7400f7
enoqueJonas <enoque.macanda@gmail.com>
2024-02-24 12:16:27 +0200

    feat: enable only signned in users to view posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,33 @@
 class PostsController < ApplicationController
   before_action :require_login, only: [:new, :create]
+  def index
+    @posts = Post.all
+  end
 
   def new
-
+    @post = Post.new
+    @user = current_user
   end
 
   def create
+    @post = Post.new(posts_params)
+    @post.user = User.find(params[:user_id])
 
+    if @post.save
+      flash[:success] = "Post created successfully"
+      redirect_to user_posts_path
+    else
+      render :new, error: :unprocessable_entity
+    end
+  end
+
+  def posts_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def require_login
+    unless user_signed_in?
+      redirect_to new_user_session, alert: "You have to be signed in to create a post!"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users
-  resources :posts
+  resources :users do
+    resources :posts
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "posts#index"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
